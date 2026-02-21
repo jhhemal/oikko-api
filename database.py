6a7986babe616c30bbbf3aa10f35b8dcfc65a34c
Jahidul Hasan <jhhemal@mac.lan>
2026-02-21 16:47:53 -0500

    Fixed Postgresql-v3

--- database.py
+++ database.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
     DATABASE_URL,
